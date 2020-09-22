825f8e00f92a49c39a0b15aade1a49651dbaeff0
ironkik <soufianetalssi99@gmail.com>
2020-09-22 15:51:36 +0200

    Updates

---
+++ precache-manifest.e19e637cbdec3a037e12fc9260127740.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8ff81c707a26eea77f5fdc8618fe5b",
+    "url": "/react--app/index.html"
+  },
+  {
+    "revision": "c7bd655e5f9ee81b530a",
+    "url": "/react--app/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "e2c643cface9ccd4010c",
+    "url": "/react--app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "c7bd655e5f9ee81b530a",
+    "url": "/react--app/static/js/2.db15f912.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react--app/static/js/2.db15f912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c643cface9ccd4010c",
+    "url": "/react--app/static/js/main.eb966eed.chunk.js"
+  },
+  {
+    "revision": "ea1f4655824d34d60ff1",
+    "url": "/react--app/static/js/runtime-main.f2aca6f9.js"
+  }
+]);
